5299f8503622f26a447620ed759d1096ede46b74
Logan Anderson <voiceoverlogan@gmail.com>
2025-02-02 18:53:30 -0800

    Initial commit

---
+++ src/main/java/net/lomacanderson/navarchy/NavarchyClient.java
@@ -0,0 +1,10 @@
+package net.lomacanderson.navarchy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NavarchyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/lomacanderson/navarchy/NavarchyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lomacanderson.navarchy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NavarchyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
